fe6a2dfe3b5a38302555d5d6e1521ff1017a5ce0
montyplexer <73080205+montyplexer@users.noreply.github.com>
2023-07-19 20:06:16 -0400

    Added menu to expression evaluator

--- src/expression_evaluator.cpp
+++ src/expression_evaluator.cpp
@@ -7,34 +7,59 @@
 using namespace std;
 
 int main (int argc, char* argv[]) {
-    bool is_running = true;
     string user_input;
+    int eval_mode = 1;
     cout << "Entered basic expression evaluator." << endl;
     /* Lexer */
     Lexer lexer = Lexer();
     /* Tokens generated during lexing */
     vector<token_t> tokens = vector<token_t>();
-    /* Parser */
+    
+    while (true) {
+        while (true) {
+            cout << endl << "=== MENU ===" << endl;
+            cout << "What would you like to use?" << endl;
+            cout << "1. Lexer: display generated tokens" << endl;
+            cout << "2. Lexer > Parser: display abstract syntax tree" << endl << endl;
 
-    while (is_running) {
-        /* Clear token list */
-        tokens.clear();
-        
-        /* Query user */
-        cout << "> ";
-        getline(cin,user_input);
-        
-        /* Do stuff */
-        lexer.set_text(user_input);
-        while (lexer.is_lexing()) {
-            token_t token = lexer.next_token();
-            tokens.push_back(token);
-            cout << Lexer::print_token(token) << " ";
+            getline(cin,user_input);
+
+            if (user_input == "exit") {exit(0);}
+
+            eval_mode = atoi(user_input.c_str());
+
+            if (1 <= eval_mode && eval_mode <= 1) { 
+                cout << "Entering mode " << eval_mode << "..." << endl << endl;
+                break; 
+            }
+            else { cout << "Please type a number corresponding to a valid mode." << endl; }
         }
-        cout << endl;
-        cout << "Number of tokens created: " << tokens.size() << endl;
+        while (true) {
+            /* Clear token list */
+            tokens.clear();
+            
+            /* Query user */
+            cout << "> ";
+            getline(cin,user_input);
+            
+            /* Exit */
+            if (user_input == "exit") {exit(0);}
+            if (user_input == "menu") {break;}
 
-        /* Exit */
-        if (user_input == "exit") {exit(0);}
+            /* Do stuff */
+            lexer.set_text(user_input);
+            while (lexer.is_lexing()) {
+                token_t token = lexer.next_token();
+                tokens.push_back(token);
+                cout << Lexer::print_token(token) << " ";
+            }
+            cout << endl;
+            cout << "Number of tokens created: " << tokens.size() << endl;
+
+        }
     }
+
+    
+
+    
 }
